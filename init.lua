4bb0f1a10d9d449c7ab0aaf5ce305e886faf2322
Phuong Nguyen <phuongsover2@gmail.com>
2023-11-05 01:06:00 +0700

    added debugger for C/C++

--- init.lua
+++ init.lua
@@ -63,6 +63,25 @@ return {
       },
     },
   },
+  dap = {
+    adapters = {
+      cppdbg = {
+        id = "cppdbg",
+        type = "executable",
+        command = "/home/phuong/.local/share/nvim/mason/packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+      },
+    },
+    configurations = {
+      c = {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
+        cwd = "${workspaceFolder}",
+      },
+    },
+  },
+
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,9 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.nord-nvim", enabled = true }
+    { import = "astrocommunity.colorscheme.nord-nvim", enabled = true },
+    { import = "astrocommunity.motion.flash-nvim", enabled = true },
+    { import = "astrocommunity.register.nvim-neoclip-lua", enabled = true },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
