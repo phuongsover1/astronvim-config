2b8a4986e4db9ab6cb364897dcc8c5d67894e147
Phuong Nguyen <phuongsover2@gmail.com>
2023-11-05 01:06:26 +0700

    update

--- init.lua
+++ init.lua
@@ -72,12 +72,23 @@ return {
       },
     },
     configurations = {
-      c = {
-        name = "Launch",
-        type = "gdb",
+      cpp = {
+        name = "Launch file",
+        type = "cppdbg",
         request = "launch",
         program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
         cwd = "${workspaceFolder}",
+        stopAtEntry = true,
+      },
+      {
+        name = "Attach to gdbserver :1234",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerServerAddress = "localhost:1234",
+        miDebuggerPath = "/usr/bin/gdb",
+        cwd = "${workspaceFolder}",
+        program = function() return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file") end,
       },
     },
   },
