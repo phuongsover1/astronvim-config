5129d205c78638a172a88be1bbe4b2d5ba00e595
Phuong Nguyen <phuongsover2@gmail.com>
2024-03-09 00:56:01 +0700

    updaaate

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "nord",
+  colorscheme = "catppuccin",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,12 +1,23 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-    { import = "astrocommunity.colorscheme.nord-nvim", enabled = true },
+    { import = "astrocommunity.colorscheme.nord-nvim", enabled = false },
+    { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
     { import = "astrocommunity.motion.flash-nvim", enabled = true },
     { import = "astrocommunity.register.nvim-neoclip-lua", enabled = true },
+    { import = "astrocommunity.color.transparent-nvim", enabled = true },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  {
+   "catppuccin",
+    opts = {
+      background = {
+        light = "latte",
+        dark = "mocha",
+      }
+    }
+  },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
